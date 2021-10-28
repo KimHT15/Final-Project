612a679e7d872cd059649dce2d29e387e2ae6937
Kimberly Herrera <kimherrera601@gmail.com>
2021-10-28 11:31:18 -0600

    Add 8th segue

---
+++ Final Project/Screencontroller7.swift
@@ -0,0 +1,29 @@
+//
+//  Screencontroller7.swift
+//  Final Project
+//
+//  Created by Kimberly Herrera on 10/25/21.
+//
+
+import UIKit
+
+class Screencontroller7: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
