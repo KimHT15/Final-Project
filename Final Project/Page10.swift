6f93adaa2ac541af86472ac5d151f347c9b9fbff
Kimberly Herrera <kimherrera601@gmail.com>
2021-11-17 10:37:05 -0700

    all screens are done

---
+++ Final Project/Page10.swift
@@ -0,0 +1,29 @@
+//
+//  Page10.swift
+//  Final Project
+//
+//  Created by Kimberly Herrera on 11/17/21.
+//
+
+import UIKit
+
+class Page10: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
